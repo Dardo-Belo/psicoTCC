3393acf56db5336ef600395154c24364fce64ef5
Dardo <dardo_belo@hotmail.com>
2022-12-11 19:55:03 -0300

    first commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,9 +9,10 @@ return(
             <p>© 2022 Copyright</p>
             <p>Todos los derechos reservados</p>
             <div className="linkdelpie">
-                <a href="https://www.facebook.com/" className="btn btn-primary bg-light logo"><img src={Facelog} className="logoimg" alt="ir a face"/></a>
-                <a href="https://www.youtube.com/" className="btn btn-primary bg-light logo"><img src={Tubelog} className="logoimg" alt="ir a youtube"/></a>
+            <img href="https://www.facebook.com/" src={Facelog} className="logo"  alt="ir a face"/>
+            <img href="https://www.youtube.com/" src={Tubelog} className="logo" alt="ir a youtube"/>
             </div>
+
         </footer>
 )
 }
